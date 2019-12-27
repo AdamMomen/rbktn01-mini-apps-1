da7bfaafd1e9ec8e0d742d7fd21ac209016f1696
AdamMomen <adammuman81@gmail.com>
2019-12-27 14:57:06 +0100

    turning modes x-o

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,26 +4,27 @@ function play(event) {
     //create a sub funtion that will the check the rule of the next player
 
     event.firstChild.nodeValue === '-' ?
-        event.firstChild.nodeValue = "X" : ''
+        event.firstChild.nodeValue = nextPlayer() : ''
 }
 
 document.querySelectorAll(".grid-item")
     .forEach(one => {
-        one.addEventListener('click', () => { play(one) });
+        one.addEventListener('click', () => {
+            play(one)
+            Board();
+            console.log(Board)
+        });
     });
 
-//re
-// object of player movement
 
+// object of player movement
 var players = {
     'O': false,
     'X': false,
 }
 
-//input => null 
-//output  => string
+//function that alternate between X - O returns the next movement
 function nextPlayer() {
-
     if (!players['X'] && !players['O']) {
         players['X'] = !players['X']
         return 'X'
@@ -38,5 +39,12 @@ function nextPlayer() {
         players['O'] = !players['O']
         return 'X'
     }
-
+}
+//function that will scan the board and put the values in an array
+var Board = [];
+function scanBoard() {
+    return document.querySelectorAll(".grid-item")
+        .map(one => {
+            return one.firstChild.nodeValue;
+        });
 }
