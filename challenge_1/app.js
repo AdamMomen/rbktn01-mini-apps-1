e56b2d133154e84eb71dc864d49ddc326b02c216
AdamMomen <adammuman81@gmail.com>
2019-12-27 15:26:44 +0100

    implemented the scan Board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,50 +1,70 @@
+var board = {
 
-//function that adds a x to elemten
-function play(event) {
-    //create a sub funtion that will the check the rule of the next player
+  0: '-',
+  1: '-',
+  2: '-',
+  3: '-',
+  4: '-',
+  5: '-',
+  6: '-',
+  7: '-',
+  8: '-',
 
-    event.firstChild.nodeValue === '-' ?
-        event.firstChild.nodeValue = nextPlayer() : ''
 }
-
-document.querySelectorAll(".grid-item")
-    .forEach(one => {
-        one.addEventListener('click', () => {
-            play(one)
-            Board();
-            console.log(Board)
-        });
-    });
-
-
 // object of player movement
 var players = {
-    'O': false,
-    'X': false,
+  'O': false,
+  'X': false,
 }
+//function that adds a x to elemten
+function play(event, index) {
+  //create a sub funtion that will the check the rule of the next player
+  event.firstChild.nodeValue === '-' ?
+    event.firstChild.nodeValue = scanBoard(index) : ''
+}
+
+
+function scanBoard(index) {
+  if (board[index] === '-') {
+    console.log(board)
+    return board[index] = nextPlayer();
+  }
+  return;
+}
+
+
 
 //function that alternate between X - O returns the next movement
 function nextPlayer() {
-    if (!players['X'] && !players['O']) {
-        players['X'] = !players['X']
-        return 'X'
-    }
-    if (players['X'] && !players['O']) {
-        players['X'] = !players['X']
-        players['O'] = !players['O']
-        return 'O'
-    }
-    if (!players['X'] && players['O']) {
-        players['X'] = !players['X']
-        players['O'] = !players['O']
-        return 'X'
-    }
+  if (!players['X'] && !players['O']) {
+    players['X'] = !players['X']
+    return 'X'
+  }
+  if (players['X'] && !players['O']) {
+    players['X'] = !players['X']
+    players['O'] = !players['O']
+    return 'O'
+  }
+  if (!players['X'] && players['O']) {
+    players['X'] = !players['X']
+    players['O'] = !players['O']
+    return 'X'
+  }
 }
-//function that will scan the board and put the values in an array
-var Board = [];
-function scanBoard() {
-    return document.querySelectorAll(".grid-item")
-        .map(one => {
-            return one.firstChild.nodeValue;
-        });
-}
+// function that will scan the board and put the values in an array
+
+// function scanBoard() {
+//   board = document.querySelectorAll(".grid-item")
+//     .forEach((one, index) => {
+//       if (index > 8) { break };
+//       console.log(index, one.firstChild.nodeValue)
+//       board[index] = one.firstChild.nodeValue;
+//     });
+//   console.log(board)
+// }
+document.querySelectorAll(".grid-item")
+  .forEach((one, index) => {
+    one.addEventListener('click', () => {
+      play(one, index)
+    });
+  });
