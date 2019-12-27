3ffbf189967a9feef013bd07cf06f3df8d07aac8
AdamMomen <adammuman81@gmail.com>
2019-12-27 12:53:52 +0100

    Board Click Listener

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,28 @@
 
+//function that adds a x to elemten
+function play(event) {
+    //create a sub funtion that will the check the rule of the next player
 
-console.log('Hi from App.js')
+    event.firstChild.nodeValue === '-' ?
+        event.firstChild.nodeValue = "X" : ''
+}
+
+document.querySelectorAll(".grid-item")
+    .forEach(one => {
+        one.addEventListener('click', () => { play(one) });
+    });
+
+
+// object of player movement
+
+var players = {
+    'O': false,
+    'X': false,
+}
+//input => null 
+//output  => string
+function nextPlayer() {
+
+    //
+    // each time the function being envoked change the boolean
+}
