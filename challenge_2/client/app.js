b6689f51c2c1b366e33f8d59b3d89d1c5078c533
AdamMomen <adammuman81@gmail.com>
2019-12-28 11:46:12 +0100

    connedcted localserver running 3000

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
+// imported I need to get the request functiionality later!!!
+//var app = require("..server")
 
+const $submitBTN = $("#submit")
+const $input = $('#input')
 
+// click listening for submit button 
+$submitBTN.click(() => {
+    var value = $('#input').val()
+    $input.val('')
+    console.log(`submitted!!!  ${value}`)
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const $ = require('jquery');
 
+var port = process.env.PORT || 3000;
+var app = express();
 
+app.use(express.static('client'))
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
+exports.app = app;
