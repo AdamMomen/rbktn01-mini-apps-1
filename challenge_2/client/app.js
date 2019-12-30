2d6812c73f3694860bb38db6d47a0afc2429eea7
AdamMomen <adammuman81@gmail.com>
2019-12-30 09:55:47 +0100

    init

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -10,3 +10,28 @@
 //     $input.val('')
 //     console.log(`submitted!!!  ${value}`)
 // })
+
+
+// funcion that converts from JSON to CVS
+
+// input  => JSON
+// output => CVS tabled format
+
+function json2CVS(json) {
+    var result = ''
+    // helpler function 
+    function helpler(child) {
+
+        // recursion
+        // iterate over it recursively
+        for (var key in child) {
+            helpler(child.children)
+        }
+        // other iteration for convertion 
+        for (var key in child) {
+            result += child[key];
+        }
+        return result
+    }
+    helpler(json)
+}
