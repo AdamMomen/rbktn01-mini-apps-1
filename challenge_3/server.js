7aa7a16e02ecdc61e23984dff3393be25e338bf7
AdamMomen <adammuman81@gmail.com>
2019-12-30 11:12:43 +0100

    install react + babel + mpm

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
+
+var port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
